085fb0dd0cbb14b239aec584a5cb3b4dafb6c7c8
Keerthana S <keerthanas@tejasnetworks.com>
2024-08-21 10:18:15 +0530

    initial commit

---
+++ src/test/java/com/tejas/aps/ApsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejas.aps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
